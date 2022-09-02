2e18ac4ef4d4062c101c4e7d4e6b8ac71c1ed19d
Светка <sveta.chakrygina@mail.ru>
2022-09-02 23:48:36 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,21 @@
-class Homework {
-        public static void printYear(int yearLeap){
-            System.out.println(yearLeap);
+import java.time.LocalDate;
 
-        }
-        public static void main(String[] args) {
-            int year = 2021;
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                System.out.println(year + " - високосный год");
-            } else {
-                System.out.println(year + " - не високосный год");
-            }
-        }
+class Homework {
+    public static void printYearLeap(int yearLeap) {
+        System.out.println(yearLeap + " - это високосный год");
     }
 
+    public static void printNoYearLeap(int yearNoLeap) {
+        System.out.println(yearNoLeap + " - это не високосный год");
+    }
 
+    public static void main(String[] args) {
 
-
-///  public static void printTask2 ( int yearLeap){
-///     System.out.println(yearLeap);
-
-
-///   public static void printTask3 ( int yearLeap){
-///     System.out.println(yearLeap);
-
-
-          ///  public static void task2 () {
-
-            ///    int OSClient = 1;
-              ///  int clientDeviceYear = 2010;
-              ///  if (OSClient == 0) {
-              ///      if (clientDeviceYear >= 2015) {
-                 ///       System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-               ///     } else {
-                  ///      System.out.println("Приложение будет работать корректно на iOS");
-             ///       }
-             ///   } else {
-                    ///      if (clientDeviceYear < 2015) {
-                    ///      System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    ///   } else {
-                    ///      System.out.println("Установите облегченную версию приложения для Android по ссылке");
-
-                    ///       }
-                    ///     }
-                    ///    }
-                    ///  public static void task3 () {
-                    ///      int deliveryDistance = 95;
-                    ///    int deliveryDays = 1;
-                    ///    if (deliveryDistance > 20) {
-                    ///       deliveryDays++;
-                    ///    }
-                    ///    if (deliveryDistance > 60) {
-                    ///        deliveryDays++;
-                    ///    }
-                ///    System.out.println("Потребуется дней: " + deliveryDays);
-                    ///    }
-                    ///  }
-
-
-
+        int year = 2024;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            printYearLeap(year);
+        } else {
+            printNoYearLeap(year);
+        }
+    }
+}

ee6931b8c91b62244cf5f7f4103d2a016056ab4d
Светка <sveta.chakrygina@mail.ru>
2022-09-03 16:57:05 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,39 @@
 import java.time.LocalDate;
 
 class Homework {
-    public static void printYearLeap(int yearLeap) {
-        System.out.println(yearLeap + " - это високосный год");
-    }
-
-    public static void printNoYearLeap(int yearNoLeap) {
-        System.out.println(yearNoLeap + " - это не високосный год");
-    }
-
-    public static void main(String[] args) {
-
-        int year = 2024;
+    public static void printYearLeap(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            printYearLeap(year);
+            System.out.println(year + " - это високосный год");
         } else {
-            printNoYearLeap(year);
+            System.out.println(year + " - это не високосный год");
+
         }
     }
-}
+
+    public static int getClientOS(String name) {
+        if (name.equals("iOS")) {
+                System.out.println(name + "система");
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        public static void main (String[]args){
+            int year = 1900;
+            printYearLeap(year);
+        }
+        String osName = "iOS";
+        int currentYear = LocalDate.now().getYear();
+        int clientOS = getClientOS(osName);
+        }
+
+
+
+
+
+
+
+
+
 

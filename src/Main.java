d776ac63bdcdacfc2891dc3ed6ec9bc4b9458790
Светка <sveta.chakrygina@mail.ru>
2022-09-04 13:49:06 +0300

    Задачи №1, 2

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,18 @@ class Homework {
     }
 
     public static int getClientOS(int clientOS, int currentYear) {
-        if (clientOS == 0 && currentYear < 2022) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        } else {
+        if (currentYear < 2022) {
+            System.out.println("Установите облегченную версию приложения по ссылке.");
+        }else{
+        if (clientOS == 0) {
             System.out.println("Приложение будет работать корректно для iOS.");
-            if (clientOS == 1 && currentYear > 2022) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            } else {
+            return 0;
+        } else {
+            if (clientOS == 1) {
                 System.out.println("Приложение будет работать корректно для Android.");
             }
-            return clientOS;
+                return 1;
+            }
         }
         return clientOS;
     }
@@ -31,10 +33,13 @@ class Homework {
 
         int clientOS = 0;
         int currentYear = LocalDate.now().getYear();
-        getClientOS(clientOS, currentYear);
+                getClientOS(clientOS, currentYear);
 
-    }
-}
+            }
+        }
+
+
+
 
 
 
